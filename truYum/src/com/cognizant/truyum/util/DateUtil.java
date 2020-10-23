5f7dacd8b37f17afdda016c2a3ebb7e19c37ac89
AnushaMS-98 <2016anusha@gmail.com>
2020-10-23 10:35:28 +0530

    truyum  model upd

--- truYum/src/com/cognizant/truyum/util/DateUtil.java
+++ truYum/src/com/cognizant/truyum/util/DateUtil.java
@@ -1,5 +1,20 @@
 package com.cognizant.truyum.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateUtil {
+	public static Date convertToDate(String date) {
+		Date dte = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			dte = formatter.parse(date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dte;
+	}
 
 }
